cadb423da93023b685f95b94abcf1cb1b8921d52
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 19:54:46 +0530

    Add visit rest controller

---
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitRestController.java
@@ -0,0 +1,19 @@
+package com.ceyentra.visitor_management_system.rest;
+
+import com.ceyentra.visitor_management_system.service.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class VisitRestController {
+
+    private VisitService visitService;
+
+    @Autowired
+    public VisitRestController(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+}

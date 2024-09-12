0ee8d1751fd8a2e5814444da1219912617f39c00
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 12:47:01 +0530

    Add visitor service

---
+++ src/main/java/com/ceyentra/visitor_management_system/service/VisitorService.java
@@ -0,0 +1,19 @@
+package com.ceyentra.visitor_management_system.service;
+
+import com.ceyentra.visitor_management_system.entity.Visitor;
+
+import java.util.List;
+
+public interface VisitorService {
+
+    void save(Visitor visitor);
+
+    void update(Visitor visitor);
+
+    void delete(Integer id);
+
+    Visitor findById(Integer id);
+
+    List<Visitor> findAllVisitors();
+    
+}

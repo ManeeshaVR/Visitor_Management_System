878e8738e8262c6d924f47acc6b497f6076187a0
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 12:42:28 +0530

    Check visitor health

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
@@ -1,12 +1,16 @@
 package com.ceyentra.visitor_management_system.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/visitors")
 public class VisitorRestController {
 
-    
+    @GetMapping("/health")
+    public String checkHealth(){
+        return "Hello Visitor";
+    }
 
 }

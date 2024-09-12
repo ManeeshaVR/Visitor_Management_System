d99f5385ae9cbc45fba017a0468a530f20568f2d
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 13:15:36 +0530

    Modify visitor service

--- src/main/java/com/ceyentra/visitor_management_system/service/VisitorServiceImpl.java
+++ src/main/java/com/ceyentra/visitor_management_system/service/VisitorServiceImpl.java
@@ -4,6 +4,7 @@ import com.ceyentra.visitor_management_system.dao.VisitorDAO;
 import com.ceyentra.visitor_management_system.entity.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,16 +18,19 @@ public class VisitorServiceImpl implements VisitorService{
         this.visitorDAO = visitorDAO;
     }
 
+    @Transactional
     @Override
     public Visitor save(Visitor visitor) {
         return visitorDAO.save(visitor);
     }
 
+    @Transactional
     @Override
     public Visitor update(Visitor visitor) {
         return visitorDAO.update(visitor);
     }
 
+    @Transactional
     @Override
     public void delete(Integer id) {
         visitorDAO.delete(id);

f1d3a175264004cbfe68d90271e2b816e5e59ff7
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 13:07:13 +0530

    Save visitor

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
@@ -1,16 +1,30 @@
 package com.ceyentra.visitor_management_system.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ceyentra.visitor_management_system.entity.Visitor;
+import com.ceyentra.visitor_management_system.service.VisitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/visitors")
+@RequestMapping("/api")
 public class VisitorRestController {
+
+    private VisitorService visitorService;
+
+    @Autowired
+    public VisitorRestController(VisitorService visitorService) {
+        this.visitorService = visitorService;
+    }
 
     @GetMapping("/health")
     public String checkHealth(){
         return "Hello Visitor";
+    }
+
+    @PostMapping("/visitors")
+    public Visitor saveVisitor(@RequestBody Visitor visitor){
+        visitor.setVisitorId(0);
+        return visitorService.save(visitor);
     }
 
 }

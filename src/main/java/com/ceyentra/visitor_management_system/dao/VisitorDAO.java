e616b2eb2e5203e806508d8a4883bdcb93de8300
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 13:15:49 +0530

    Modify visitor dao

--- src/main/java/com/ceyentra/visitor_management_system/dao/VisitorDAO.java
+++ src/main/java/com/ceyentra/visitor_management_system/dao/VisitorDAO.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface VisitorDAO {
 
     Visitor save(Visitor visitor);
--- src/main/java/com/ceyentra/visitor_management_system/dao/VisitorDAOImpl.java
+++ src/main/java/com/ceyentra/visitor_management_system/dao/VisitorDAOImpl.java
@@ -4,10 +4,12 @@ import com.ceyentra.visitor_management_system.entity.Visitor;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public class VisitorDAOImpl implements VisitorDAO{
 
     private EntityManager entityManager;

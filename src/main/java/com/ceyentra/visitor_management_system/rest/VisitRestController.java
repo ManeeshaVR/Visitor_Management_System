b02a884b5737e8d3ecb6ca1218a1f26ded2287a4
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 19:58:48 +0530

    Check visit controller

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitRestController.java
@@ -2,6 +2,7 @@ package com.ceyentra.visitor_management_system.rest;
 
 import com.ceyentra.visitor_management_system.service.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class VisitRestController {
     @Autowired
     public VisitRestController(VisitService visitService) {
         this.visitService = visitService;
+    }
+
+    @GetMapping("/check")
+    public String checkHealth(){
+        return "Hello Visit";
     }
 
 }

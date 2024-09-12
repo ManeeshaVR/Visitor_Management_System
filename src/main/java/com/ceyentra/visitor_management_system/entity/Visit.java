410128f83362bb71d36239d8d89a8cb971d3f01f
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 19:37:52 +0530

    Add visit entity

---
+++ src/main/java/com/ceyentra/visitor_management_system/entity/Visit.java
@@ -0,0 +1,40 @@
+package com.ceyentra.visitor_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "visit")
+@Entity
+public class Visit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "visit_id")
+    private int visitId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "visitor_id", nullable = false)
+    private Visitor visitor;
+
+    @Column(name = "visit_date", nullable = false)
+    private LocalDate visitDate;
+
+    @Column(name = "check_in", nullable = false)
+    private LocalTime checkIn;
+
+    @Column(name = "check_out", nullable = false)
+    private LocalTime checkOut;
+
+    @Column(name = "purpose", length = 255)
+    private String purpose;
+
+
+}

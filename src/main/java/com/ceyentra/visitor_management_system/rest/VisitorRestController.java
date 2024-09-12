f0eb0e86789452c86e4c481bae7846df42d665a4
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-12 12:28:54 +0530

    Add visitor rest controller

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
@@ -1,5 +1,12 @@
 package com.ceyentra.visitor_management_system.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class VisitorRestController {
+
+    
+
 }

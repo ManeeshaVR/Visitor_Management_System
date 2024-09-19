435c22a11984eefc2e938b0863eb7e242ab549ce
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-19 09:52:25 +0530

    Set Custom Query to get available cards

--- src/main/java/com/ceyentra/visitor_management_system/dao/CardDAO.java
+++ src/main/java/com/ceyentra/visitor_management_system/dao/CardDAO.java
@@ -2,8 +2,13 @@ package com.ceyentra.visitor_management_system.dao;
 
 import com.ceyentra.visitor_management_system.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardDAO extends JpaRepository<Card, Integer> {
+
+    @Query("SELECT c FROM Card c WHERE c.status = 'Available' ORDER BY c.cardNo ASC LIMIT 1")
+    Card findAvailableCard();
+
 }

4c0571c4b0a50968c31e682f5eb98346079cb936
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-20 11:28:49 +0530

    Add custom query to get visits by visitor nic

--- src/main/java/com/ceyentra/visitor_management_system/dao/VisitDAO.java
+++ src/main/java/com/ceyentra/visitor_management_system/dao/VisitDAO.java
@@ -3,9 +3,16 @@ package com.ceyentra.visitor_management_system.dao;
 import com.ceyentra.visitor_management_system.entity.Visit;
 import com.ceyentra.visitor_management_system.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VisitDAO extends JpaRepository<Visit, Integer> {
+
+    @Query("SELECT v FROM Visit v WHERE v.visitor.nic = :nic")
+    List<Visit> findVisitsByVisitorNic(@Param("nic") String nic);
 
 }

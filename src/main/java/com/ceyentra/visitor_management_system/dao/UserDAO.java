b57489faa91d0ca13d010f822069a705d0fc7408
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-25 08:45:25 +0530

    Update user details

--- src/main/java/com/ceyentra/visitor_management_system/dao/UserDAO.java
+++ src/main/java/com/ceyentra/visitor_management_system/dao/UserDAO.java
@@ -5,9 +5,13 @@ import com.ceyentra.visitor_management_system.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
 
-    User findByUserName(String userName);
+    Optional<User> findByUserName(String userName);
+
+    boolean existsByUserName(String email);
 
 }
--- src/main/java/com/ceyentra/visitor_management_system/entity/User.java
+++ src/main/java/com/ceyentra/visitor_management_system/entity/User.java
@@ -2,28 +2,27 @@ package com.ceyentra.visitor_management_system.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@Entity
+@Data
+@Builder
 @Table(name = "user")
-public class User{
+@Entity
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private String userId;
+    private Integer userId;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -33,6 +32,36 @@ public class User{
 
     @Column(name = "password", unique = true, nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
 

5a491ea637a3a5ddef090694e4efe616032514b5
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-26 21:46:31 +0530

    Add employee service

---
+++ src/main/java/com/ceyentra/visitor_management_system/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.ceyentra.visitor_management_system.service;
+
+import com.ceyentra.visitor_management_system.entity.Employee;
+import com.ceyentra.visitor_management_system.entity.Visitor;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+    Employee updateEmployee(Employee employee);
+
+    void deleteEmployee(Integer id);
+
+    Employee findEmployeeById(Integer id);
+
+    List<Employee> findAllVisitors();
+
+    List<Employee> findExistingEmployees();
+
+    List<Employee> findExistedEmployees();
+
+}
---
+++ src/main/java/com/ceyentra/visitor_management_system/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,69 @@
+package com.ceyentra.visitor_management_system.service.impl;
+
+import com.ceyentra.visitor_management_system.dao.EmployeeDAO;
+import com.ceyentra.visitor_management_system.entity.Employee;
+import com.ceyentra.visitor_management_system.entity.Visitor;
+import com.ceyentra.visitor_management_system.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
+        this.employeeDAO = employeeDAO;
+    }
+
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee) {
+        Optional<Employee> existingEmployee = employeeDAO.findById(employee.getId());
+        existingEmployee.get().setName(employee.getName());
+        existingEmployee.get().setPhoneNo(employee.getPhoneNo());
+        existingEmployee.get().setEmail(employee.getEmail());
+        existingEmployee.get().setPosition(employee.getPosition());
+        existingEmployee.get().setStatus(employee.getStatus());
+
+        return employeeDAO.getReferenceById(employee.getId());
+    }
+
+    @Override
+    public void deleteEmployee(Integer id) {
+        Optional<Employee> existingEmployee = employeeDAO.findById(id);
+        existingEmployee.get().setStatus("DEACTIVE");
+    }
+
+    @Override
+    public Employee findEmployeeById(Integer id) {
+        return employeeDAO.getReferenceById(id);
+    }
+
+    @Override
+    public List<Employee> findAllVisitors() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public List<Employee> findExistingEmployees() {
+        return employeeDAO.findActiveEmployees();
+    }
+
+    @Override
+    public List<Employee> findExistedEmployees() {
+        return employeeDAO.findDeActiveEmployees();
+    }
+}

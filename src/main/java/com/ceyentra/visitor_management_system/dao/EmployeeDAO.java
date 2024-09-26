65dccb2acb782b68ed7d18f397977ed8fd7ef12a
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-26 21:46:09 +0530

    Add employee repository

---
+++ src/main/java/com/ceyentra/visitor_management_system/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package com.ceyentra.visitor_management_system.dao;
+
+import com.ceyentra.visitor_management_system.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e FROM Employee e WHERE e.status = 'ACTIVE'")
+    List<Employee> findActiveEmployees();
+
+    @Query("SELECT e FROM Employee e WHERE e.status = 'DEACTIVE'")
+    List<Employee> findDeActiveEmployees();
+
+}

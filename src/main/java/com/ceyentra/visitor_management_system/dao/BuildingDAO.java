d106d558ec91216b8993368c2a260baf8aa4f1ec
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-26 23:17:41 +0530

    Add building dao

---
+++ src/main/java/com/ceyentra/visitor_management_system/dao/BuildingDAO.java
@@ -0,0 +1,7 @@
+package com.ceyentra.visitor_management_system.dao;
+
+import com.ceyentra.visitor_management_system.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingDAO extends JpaRepository<Building, Integer> {
+}

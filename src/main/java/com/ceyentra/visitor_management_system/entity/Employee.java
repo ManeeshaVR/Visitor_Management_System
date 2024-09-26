509d728f58dedc5cdea9a7d01f2b4706c52d0a3a
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-26 22:59:17 +0530

    Update employee with building

--- src/main/java/com/ceyentra/visitor_management_system/entity/Employee.java
+++ src/main/java/com/ceyentra/visitor_management_system/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.ceyentra.visitor_management_system.entity;
 
+import com.ceyentra.visitor_management_system.entity.enums.Floor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,12 +12,13 @@ import lombok.NoArgsConstructor;
 @Data
 @Table(name = "employee")
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
-    private int id;
+    private Integer id;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -31,5 +34,12 @@ public class Employee {
 
     @Column(name = "status", nullable = false, length = 10)
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "building_id", nullable = false)
+    private Building building;
+
+    @Column(name = "floor", nullable = false)
+    private Floor floor;
 
 }

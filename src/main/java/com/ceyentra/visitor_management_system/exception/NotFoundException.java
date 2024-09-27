a654f9886cdddb1166e83a7dcca9f3945ed05610
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-27 10:22:48 +0530

    Add notfound exception

---
+++ src/main/java/com/ceyentra/visitor_management_system/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ceyentra.visitor_management_system.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}

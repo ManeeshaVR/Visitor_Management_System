cd839aa708bf0f9692029ddcc9ca5c943d551b12
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-09-27 09:47:50 +0530

    Add regex

--- src/main/java/com/ceyentra/visitor_management_system/entity/Employee.java
+++ src/main/java/com/ceyentra/visitor_management_system/entity/Employee.java
@@ -3,6 +3,7 @@ package com.ceyentra.visitor_management_system.entity;
 import com.ceyentra.visitor_management_system.entity.enums.Floor;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,9 +24,11 @@ public class Employee {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Pattern(regexp = "^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$", message = "Invalid mobile number format")
     @Column(name = "phone_no", nullable = false, length = 12)
     private String phoneNo;
 
+    @Pattern(regexp = "(^[a-zA-Z0-9_.-]+)@([a-zA-Z]+)([\\.])([a-zA-Z]+)$", message = "Invalid email format")
     @Column(name = "email", nullable = false, length = 100)
     private String email;
 

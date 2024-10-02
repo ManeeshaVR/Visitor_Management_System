55ba7d5e2a64e4d661cf830a24a29e5bcb546188
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-10-02 17:04:09 +0530

    Organize visitor rest controller

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitorRestController.java
@@ -1,8 +1,6 @@
 package com.ceyentra.visitor_management_system.rest;
 
 import com.ceyentra.visitor_management_system.dto.VisitorDTO;
-import com.ceyentra.visitor_management_system.entity.Visitor;
-import com.ceyentra.visitor_management_system.exception.NotFoundException;
 import com.ceyentra.visitor_management_system.service.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,57 +19,40 @@ public class VisitorRestController {
     }
 
     @GetMapping("/health")
-    public String checkHealth(){
+    public String checkHealth() {
         return "Hello Visitor";
     }
 
     @PostMapping("/visitors")
-    public VisitorDTO saveVisitor(@RequestBody VisitorDTO visitor){
+    public VisitorDTO saveVisitor(@RequestBody VisitorDTO visitor) {
         visitor.setVisitorId(0);
         return visitorService.saveVisitor(visitor);
     }
 
     @PutMapping("/visitors/{visitorId}")
-    public VisitorDTO updateVisitor(@PathVariable int visitorId, @RequestBody VisitorDTO visitor){
-        if (visitorService.existsVisitor(visitorId)){
-            visitor.setVisitorId(visitorId);
-            return visitorService.updateVisitor(visitor);
-        }else {
-            throw new NotFoundException("Visitor not found with visitor ID : "+visitorId);
-        }
+    public VisitorDTO updateVisitor(@PathVariable int visitorId, @RequestBody VisitorDTO visitor) {
+        visitor.setVisitorId(visitorId);
+        return visitorService.updateVisitor(visitor);
     }
 
     @DeleteMapping("/visitors/{visitorId}")
-    public void deleteVisitor(@PathVariable int visitorId){
-        if (visitorService.existsVisitor(visitorId)){
-            visitorService.deleteVisitor(visitorId);
-        }else {
-            throw new NotFoundException("Visitor not found with visitor ID : "+visitorId);
-        }
+    public void deleteVisitor(@PathVariable int visitorId) {
+        visitorService.deleteVisitor(visitorId);
+
     }
 
     @GetMapping("/visitors/{visitorId}")
-    public VisitorDTO getVisitor(@PathVariable int visitorId){
-        if (visitorService.existsVisitor(visitorId)){
+    public VisitorDTO getVisitor(@PathVariable int visitorId) {
             return visitorService.findVisitorById(visitorId);
-        }else {
-            throw new NotFoundException("Visitor not found with visitor ID : "+visitorId);
-        }
-
     }
 
     @GetMapping("/visitors/nic/{nic}")
-    public VisitorDTO getVisitorByNic(@PathVariable String nic){
-        VisitorDTO visitorByNic = visitorService.findVisitorByNic(nic);
-        if (visitorByNic != null){
-            return visitorByNic;
-        }else {
-            throw new NotFoundException("Visitor not found with visitor NIC : "+nic);
-        }
+    public VisitorDTO getVisitorByNic(@PathVariable String nic) {
+        return visitorService.findVisitorByNic(nic);
     }
 
     @GetMapping("/visitors")
-    public List<VisitorDTO> getAllVisitors(){
+    public List<VisitorDTO> getAllVisitors() {
         return visitorService.findAllVisitors();
     }
 

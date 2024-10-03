e6fb304107c1976e8223198040517a3a8f518de2
Vimu05 <123923720+Vimu05@users.noreply.github.com>
2024-10-03 13:14:09 +0530

    Add response entity

--- src/main/java/com/ceyentra/visitor_management_system/rest/VisitRestController.java
+++ src/main/java/com/ceyentra/visitor_management_system/rest/VisitRestController.java
@@ -1,8 +1,13 @@
 package com.ceyentra.visitor_management_system.rest;
 
 import com.ceyentra.visitor_management_system.dto.VisitDTO;
+import com.ceyentra.visitor_management_system.exception.ResourceNotFoundException;
 import com.ceyentra.visitor_management_system.service.VisitService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +17,7 @@ import java.util.List;
 public class VisitRestController {
 
     private VisitService visitService;
+    private static final Logger logger = LoggerFactory.getLogger(VisitRestController.class);
 
     @Autowired
     public VisitRestController(VisitService visitService) {
@@ -24,36 +30,76 @@ public class VisitRestController {
     }
 
     @PostMapping("/visits/{visitorId}/{employeeId}")
-    public VisitDTO saveVisit(@PathVariable int visitorId, @PathVariable int employeeId, @RequestBody String purpose) {
-        VisitDTO visit = new VisitDTO();
-        visit.setVisitId(0);
-        visit.setPurpose(purpose);
-        return visitService.saveVisit(visit, employeeId, visitorId);
+    public ResponseEntity<?> saveVisit(@PathVariable int visitorId, @PathVariable int employeeId, @RequestBody String purpose) {
+        logger.info("Received request for save a visit");
+        try {
+            VisitDTO visit = new VisitDTO();
+            visit.setVisitId(0);
+            visit.setPurpose(purpose);
+            return ResponseEntity.ok(visitService.saveVisit(visit, employeeId, visitorId));
+        } catch (Exception e) {
+            logger.error("An exception occurred: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/visits/{visitId}")
-    public VisitDTO updateVisit(@PathVariable int visitId) {
-        return visitService.updateVisit(visitId);
+    public ResponseEntity<?> updateVisit(@PathVariable int visitId) {
+        logger.info("Received request for update a visit");
+        try {
+            return ResponseEntity.ok(visitService.updateVisit(visitId));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            logger.error("An exception occurred: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/visits/{visitId}")
-    public VisitDTO getVisit(@PathVariable int visitId) {
-        return visitService.findVisitById(visitId);
+    public ResponseEntity<?> getVisit(@PathVariable int visitId) {
+        logger.info("Received request for get a visit");
+        try {
+            return ResponseEntity.ok(visitService.findVisitById(visitId));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            logger.error("An exception occurred: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/visits")
-    public List<VisitDTO> getAllVisits() {
-        return visitService.findAllVisits();
+    public ResponseEntity<?> getAllVisits() {
+        logger.info("Received request to get all visits");
+        try {
+            return ResponseEntity.ok(visitService.findAllVisits());
+        } catch (Exception e) {
+            logger.error("An exception occurred : {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/visits/nic/{visitorNic}")
-    public List<VisitDTO> getVisitsByNic(@PathVariable String visitorNic) {
-        return visitService.findVisitByVisitorNic(visitorNic);
+    public ResponseEntity<?> getVisitsByNic(@PathVariable String visitorNic) {
+        logger.info("Received request to get visits by NIC");
+        try {
+            return ResponseEntity.ok(visitService.findVisitByVisitorNic(visitorNic));
+        } catch (Exception e) {
+            logger.error("An exception occurred : {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/visits/overdue")
-    public List<VisitDTO> getOverdueVisits() {
-        return visitService.findOverdueVisits();
+    public ResponseEntity<?> getOverdueVisits() {
+        logger.info("Received request to get overdue visits");
+        try {
+            return ResponseEntity.ok(visitService.findOverdueVisits());
+        } catch (Exception e) {
+            logger.error("An exception occurred : {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
